983557d9296ff1e835c5757f666269b3e08a4551
Joan <joan.quispec@gmail.com>
2022-11-01 15:17:59 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+// import dotenv from "dotenv";
+import axios from 'axios';
+import { useState, useEffect } from "react";  
 import './App.css';
+require('dotenv').config();
+const { URL} = process.env;
+// import dotenv from 'dotenv';
+// dotenv.config();
 
 function App() {
+  const [array, setArray] = useState([]);
+   const callapi = async ()=> {
+     console.log("por que",process.env)
+    const info1 = await axios.get(process.env.URL)
+    return info1
+  }
+  useEffect(() => {
+    // setArray(callapi.then);
+    callapi().then((e)=>console.log(e))
+  },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+      <>probando</>
+      {
+        array.length?
+          array.map((e)=>{
+            console.log(e);
+            return(
+              <div>e.name</div>
+            )
+          }
+          )
+        :
+          <label>NO TENGO NADA</label>
+        
+      }
     </div>
   );
 }
