cd848aa03235dbad700a4dfaa16726aae783ee50
Joan <joan.quispec@gmail.com>
2022-11-07 10:55:41 -0500

    cambios

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-// import dotenv from "dotenv";
+// import dotenv from "dotenv"; 
 import axios from 'axios';
 import { useState, useEffect } from "react";  
 import './App.css';
-require('dotenv').config();
-const { URL} = process.env;
-// import dotenv from 'dotenv';
+// require('dotenv').config();
+// const { REACT_APP_URL} process.env;
 // dotenv.config();
 
 function App() {
   const [array, setArray] = useState([]);
    const callapi = async ()=> {
-     console.log("por que",process.env)
-    const info1 = await axios.get(process.env.URL)
+     console.log("por que", process.env.REACT_APP_URL)
+    const info1 = await axios.get(process.env.REACT_APP_URL)
     return info1
   }
   useEffect(() => {
@@ -20,7 +19,7 @@ function App() {
   },[]);
 
   return (
-    <div className="App"> 
+    <div className="App">  
       <>probando</>
       {
         array.length?
