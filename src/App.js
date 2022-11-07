9e712b64e717258e424902fb8ac4dec15186001a
Angelo <rideralucar@gmail.com>
2022-11-07 10:51:43 -0500

    modificacion App

--- src/App.js
+++ src/App.js
@@ -2,38 +2,37 @@
 import axios from 'axios';
 import { useState, useEffect } from "react";  
 import './App.css';
-require('dotenv').config();
-const { URL} = process.env;
 // import dotenv from 'dotenv';
 // dotenv.config();
 
 function App() {
   const [array, setArray] = useState([]);
    const callapi = async ()=> {
-     console.log("por que",process.env)
-    const info1 = await axios.get(process.env.URL)
+    const info1 = await axios.get(process.env.REACT_APP_URL_BACK)
+    setArray(info1.data)
     return info1
   }
   useEffect(() => {
     // setArray(callapi.then);
-    callapi().then((e)=>console.log(e))
+    callapi();
   },[]);
+//background_image
 
+console.log(array)
   return (
     <div className="App"> 
-      <>probando</>
       {
         array.length?
           array.map((e)=>{
-            console.log(e);
+            console.log(e.background_image);
             return(
-              <div>e.name</div>
+              <div>
+               <img style={{width:"50rem"}} src={e.background_image} alt="MDN"/></div>
             )
           }
           )
         :
           <label>NO TENGO NADA</label>
-        
       }
     </div>
   );
